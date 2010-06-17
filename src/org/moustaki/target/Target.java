731fbd8b41f5219c77243acebb3de4ab8530d4f8
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-18 00:30:47 +0100

    Initial commit

---
+++ src/org/moustaki/target/Target.java
@@ -0,0 +1,33 @@
+package org.moustaki.target;
+
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class Target extends MapActivity {
+	private LocationManager lm;
+    private LocationListener ll;
+    private MapController mc;
+    private MapView mv;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        this.mv = (MapView) findViewById(R.id.mapview);
+        this.mv.setBuiltInZoomControls(true);
+        this.mc = this.mv.getController();
+        this.mc.setZoom(16);
+        this.lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        this.ll = new TargetLocationListener(this.mc, this.mv);
+        this.lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+}
---
+++ src/org/moustaki/target/TargetLocationListener.java
@@ -0,0 +1,46 @@
+package org.moustaki.target;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class TargetLocationListener implements LocationListener {
+	private MapController mc;
+	private MapView mv;
+	public TargetLocationListener(MapController mc, MapView mv) {
+		this.mc = mc;
+		this.mv = mv;
+	}
+	@Override
+	public void onLocationChanged(Location loc) {
+		if (loc != null) {                
+            GeoPoint p = new GeoPoint(
+                    (int) (loc.getLatitude() * 1E6), 
+                    (int) (loc.getLongitude() * 1E6)
+            );
+            this.mc.animateTo(p);
+            this.mc.setZoom(16);       
+            this.mv.invalidate();
+        }
+	}
+	@Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+}
