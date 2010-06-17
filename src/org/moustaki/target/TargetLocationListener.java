5b5c1a64c9730f216e94e5f9354377ff4198ddf7
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-18 00:42:14 +0100

    And another one...

--- src/org/moustaki/target/TargetLocationListener.java
+++ src/org/moustaki/target/TargetLocationListener.java
@@ -36,8 +36,8 @@ public class TargetLocationListener implements LocationListener {
     public void onProviderEnabled(String provider) {
         // TODO Auto-generated method stub
     }
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub	
-	}
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    	// TODO Auto-generated method stub	
+    }
 }
