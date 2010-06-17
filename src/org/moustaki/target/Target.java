1ae656ca29b76a0e5cb88fae58506ae0574e5504
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-18 00:44:30 +0100

    And another one...

--- src/org/moustaki/target/Target.java
+++ src/org/moustaki/target/Target.java
@@ -9,12 +9,12 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 public class Target extends MapActivity {
-	private LocationManager lm;
-	private LocationListener ll;
-	private MapController mc;
-	private MapView mv;
-	/** Called when the activity is first created. */
-	@Override
+    private LocationManager lm;
+    private LocationListener ll;
+    private MapController mc;
+    private MapView mv;
+    /** Called when the activity is first created. */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
