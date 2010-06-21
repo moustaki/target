79c0b15a02018d000f2450531b88a7beca0d41a4
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-21 12:16:43 +0100

    Game controller

--- server/src/game.py
+++ server/src/game.py
@@ -9,17 +9,23 @@ def singleton(cls):
             instance_container.append(cls())
         return instance_container[0]
     return getinstance
-
 # A singleton class wrapping the game state - perhaps worth persisting it
 @singleton
 class State:
     max_player_id = 0
+    max_game_id = 0
     players = {}
+    games = {}
 
 class GameController(webapp.RequestHandler):
     def get(self, game_id):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!' + str(game_id))
+        game_id = int(game_id)
+        state = State()
+        self.response.headers['Content-Type'] = 'application/json'
+        if game_id <= state.max_game_id:
+            self.response.out.write(simplejson.dumps(state.games[game_id]))
+        else:
+            self.response.out.write('{}')
 
 class PlayerController(webapp.RequestHandler):
     def get(self, player_id):
@@ -50,11 +56,20 @@ class RegisterController(webapp.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(simplejson.dumps(state.players[state.max_player_id]))
 
+class StartController(webapp.RequestHandler):
+    def get(self):
+        state = State()
+        state.max_game_id += 1
+        state.games[state.max_game_id] = {"id":state.max_game_id}
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(simplejson.dumps(state.games[state.max_game_id]))
+
 application = webapp.WSGIApplication(
                                      [
                                         ('/games/(\d+)', GameController),
                                         ('/players/(\d+)', PlayerController),
-                                        ('/register', RegisterController)
+                                        ('/register', RegisterController),
+                                        ('/start', StartController),
                                      ],
                                      debug=True)
 
