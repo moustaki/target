c3f0a6e21b3f5a505ad610ceb9e98978f60cfa48
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-22 23:28:39 +0100

    Pointing to app engine instead of dev server

--- src/org/moustaki/target/Target.java
+++ src/org/moustaki/target/Target.java
@@ -63,7 +63,7 @@ public class Target extends MapActivity {
         
         // Setting up game context
         // @todo - shouldn't be hardcoded here
-        this.game = new Game("http://192.168.1.67:1234");
+        this.game = new Game("http://moustaki-target.appspot.com");
         
         // Registering user
         this.game.register();
