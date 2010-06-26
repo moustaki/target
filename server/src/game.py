dd88426c099fabc7cf36e78a139d673be7fea34c
Yves Raimond <yves.raimond@bbc.co.uk>
2010-06-26 23:35:53 +0100

    Objectives support, server-side

--- server/src/game.py
+++ server/src/game.py
@@ -13,12 +13,30 @@ class Player(db.Model):
         return d
 class Game(db.Model):
     players = db.ListProperty(db.Key)
+    objectives = db.ListProperty(db.Key)
     def to_dict(self):
         d = {}
         d['id'] = self.key().id()
         d['players'] = []
         for player in db.get(self.players):
             d['players'].append(player.to_dict())
+        d['objectives'] = []
+        for objective in db.get(self.objectives):
+            d['objectives'].append(objective.to_dict())
+        return d
+class Objective(db.Model):
+    game_id = db.IntegerProperty()
+    id_in_game = db.IntegerProperty()
+    power = db.IntegerProperty()
+    latitude = db.IntegerProperty()
+    longitude = db.IntegerProperty()
+    def to_dict(self):
+        d = {}
+        d['id'] = self.key().id()
+        d['id_in_game'] = self.id_in_game
+        d['power'] = self.power
+        d['latitude'] = self.latitude
+        d['longitude'] = self.longitude
         return d
 
 class GameController(webapp.RequestHandler):
@@ -72,7 +90,32 @@ class PlayerController(webapp.RequestHandler):
         else:
             self.error(404)
             self.response.write('No such player')
-        
+
+class ObjectivesController(webapp.RequestHandler):
+    def post(self):
+        game_id = int(self.request.get('game_id'))
+        game = Game.get_by_id(game_id)
+        if game:
+            id_in_game = int(self.request.get('id_in_game'))
+            power = int(self.request.get('power'))
+            latitude = int(self.request.get('latitude'))
+            longitude = int(self.request.get('longitude')) 
+            objective = Objective()
+            objective.game_id = game_id
+            objective.id_in_game = id_in_game
+            objective.power = power
+            objective.latitude = latitude
+            objective.longitude = longitude
+            objective.put()
+            if objective.key() not in game.objectives:
+                game.objectives.append(objective.key())
+                game.put()
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.headers['Content-Location'] = '/objectives/' + str(objective.key().id())
+            self.response.out.write(simplejson.dumps(objective.to_dict()))
+        else:
+            self.error(412)
+            self.response.out.write('No such game')
 
 class RegisterController(webapp.RequestHandler):
     def get(self):
@@ -94,6 +137,7 @@ application = webapp.WSGIApplication(
                                         ('/players/(\d+)', PlayerController),
                                         ('/register', RegisterController),
                                         ('/start', StartController),
+                                        ('/objectives', ObjectivesController)
                                      ],
                                      debug=True)
 
